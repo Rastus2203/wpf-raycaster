fbac5e3ce1a71d9091963507740d2b239ba349a6
Tom Boehmer <rastus2203@gmail.com>
2020-09-17 15:33:13 +1000

    Started writing

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RayCaster
+{
+    class rayCaster
+    {
+        int frameHeight;
+        int frameWidth;
+        int bytesPerPixel;
+        int frameStride;
+        byte[] frameBuffer;
+
+        public rayCaster (int inputHeight, int inputWidth, int inputBytesPerPixel)
+        {
+            frameWidth = inputWidth;
+            frameHeight = inputHeight;
+            bytesPerPixel = inputBytesPerPixel;
+            frameStride = frameWidth * bytesPerPixel;
+
+            frameBuffer = new byte[frameWidth * frameHeight * bytesPerPixel];
+
+
+
+
+        }
+
+
+
+
+
+        public void drawSolidWall(ref byte[] frameBuffer, byte[] colour, int x, int width, int height)
+        {
+                        
+        }
+
+
+
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,7 @@ using System.Windows.Shapes;
 
 namespace RayCaster
 {
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
@@ -24,11 +26,35 @@ namespace RayCaster
         {
             InitializeComponent();
 
-            BitmapSource frame; //test1
+            int frameHeight = 900;
+            int frameWidth = 1600;
+            int bytesPerPixel = 4;
 
+
+            rayCaster objRaycaster = new rayCaster(frameHeight, frameWidth, bytesPerPixel);
+
+            
+            // Draws a gradient over the back, mostly just testing if viewport works
+            for (int i = 0; i < frameBuffer.Length; i++)
+            {
+                float multiplier = (float)i / frameBuffer.Length;
+                if (i % 4 < 3) {
+                    frameBuffer[i] = (byte)(0xff * multiplier);
+                } else if (i % 4 == 3) {
+                    frameBuffer[i] = 0xff;
+                }
+            }
+
+
+            frame = BitmapSource.Create(frameWidth, frameHeight, 96, 96, PixelFormats.Bgra32, null, frameBuffer, frameStride);
+            frame.Freeze();
+            viewPort.Source = frame;
 
 
 
         }
     }
+
+
+
 }
